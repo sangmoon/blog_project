93a8f097c8d3d0bfd37c40c36a9833c64130987e
parksangmoon <cholsoo22001@gmail.com>
2016-06-28 10:24:13 +0900

    fixed Article model

--- apps/models.py
+++ apps/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
 # Create your models here.
 
 
 class Article(models.Model):
 
     user = models.ForeignKey(User, default=None)
-    title = models.CharField(max_length=200)
-    created_time = models.DateTimeField(default=datetime.now)
-    content = models.TextField()
+    title = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField(max_length=2048)
 
     class Meta:
-        ordering = ['-created_time']
+        ordering = ['-created_at']
