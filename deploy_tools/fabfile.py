e7854f121ba043ef3370e019592de8ea6fedf41c
parksangmoon <cholsoo22001@gmail.com>
2016-06-29 16:57:37 +0900

    fixed fabric error. now it works!

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -18,7 +18,7 @@ def deploy():
 
 def _create_directory_structure_if_necessary(site_folder):
     for subfolder in ('database', 'static', 'virtualenv', 'source'):
-        run('mkdir -p %s%s' % (site_folder, subfolder))
+        run('mkdir -p %s/%s' % (site_folder, subfolder))
 
 
 def _get_latest_source(source_folder):
@@ -34,20 +34,19 @@ def _update_settings(source_folder, site_name):
     settings_path = source_folder + '/myblog/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")
     sed(settings_path,
-        'ALLOWED_HOST =.+$',
-        'ALLOWED_HOST = ["%s"]' % (site_name,)
-        )
-    secret_key_file = source_folder + 'myblog/secret_key.py'
+        'ALLOWED_HOSTS =.+$',
+        'ALLOWED_HOSTS = ["%s"]' % (site_name,))
+    secret_key_file = source_folder + '/myblog/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
-        append(secret_key_file, "SECRET_KEY = '%S'" % (key,))
+        append(secret_key_file, "SECRET_KEY = '%s'" % (key,))
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
 
 def _update_virtualenv(source_folder):
     virtualenv_folder = source_folder + '/../virtualenv'
-    if not exists(virtualenv_folder + 'bin/pip'):
+    if not exists(virtualenv_folder + '/bin/pip'):
         run('virtualenv --python=python3 %s' % (virtualenv_folder,))
     run('%s/bin/pip install -r %s/requirements.txt' % (
         virtualenv_folder, source_folder
