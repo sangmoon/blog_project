9740995eabb81a4dcea39b4704fea964a3ba2bbc
parksangmoon <cholsoo22001@gmail.com>
2016-07-24 19:18:15 +0900

    form 계속 수정 중

--- apps/forms.py
+++ apps/forms.py
@@ -4,6 +4,19 @@ from .models import Article
 
 
 class ArticleForm(forms.ModelForm):
+
     class Meta:
         model = Article
+        fields = ('title', 'content',)
         exclude = ('user', 'created_at', 'updated_at',)
+        widgets = {
+            'title': forms.fields.TextInput(attrs={
+                'placeholder': 'title',
+                'class': 'form-control input-md',
+            }),
+
+            'content': forms.Textarea(attrs={
+                'placeholder': 'content',
+                'class': 'form-control input-md',
+            }),
+        }
