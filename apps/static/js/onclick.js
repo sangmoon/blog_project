1bfbdc49f683edcca027024abfbaec161e1d30bd
parksangmoon <cholsoo22001@gmail.com>
2016-12-22 17:52:15 +0900

    if press button, display:none for md preview

--- apps/static/js/onclick.js
+++ apps/static/js/onclick.js
@@ -1,7 +1,33 @@
-
-window.onload = function(){
-    var hw = document.getElementById('hw');
-    hw.addEventListener("click", function(){
-        alert('Hello world')
+function markdownPreview() {
+    var pb = document.getElementById('preview_button');
+    var target = document.getElementById('id_content');
+    pb.addEventListener("click", function(){
+        //alert('Hello world');
+        target.style.display = 'none';
     })
 }
+
+function recover2WritingForm() {
+    var wb = document.getElementById('write_button');
+    var target = document.getElementById('id_content');
+    wb.addEventListener("click", function() {
+        target.style.display = 'inline';
+    })
+}
+
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+           if(oldonload) {
+                oldonload();
+            }
+            func();
+        }
+    }
+}
+
+addLoadEvent(markdownPreview);
+addLoadEvent(recover2WritingForm);
