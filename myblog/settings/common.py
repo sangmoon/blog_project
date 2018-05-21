70e5bc8eb1a031b3c987158e2caa79a5596f059e
sangmoon <cholsoo22001@gmail.com>
2018-05-21 15:41:15 +0900

    staticfile_dirs추가

--- myblog/settings/common.py
+++ myblog/settings/common.py
@@ -109,6 +109,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps', 'static')]
+
 
 # for login/ logout
 LOGIN_URL = "/login"
