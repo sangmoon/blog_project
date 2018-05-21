92c167b92b8a15fd1434bdfa9761f822c7d9654e
sangmoon <cholsoo22001@gmail.com>
2018-05-21 15:48:41 +0900

    admin 삭제 . base_dir 수정

--- myblog/settings/common.py
+++ myblog/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'apps', 'static'),]
 
 
 # for login/ logout
